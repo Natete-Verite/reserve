70bb744a0d49d0da977b9c21a2ef07ea591b280a
Natete-Verite <nateteverite@gmail.com>
2022-11-15 18:29:27 +0300

    fetching data from firestore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cards from "../components/Cards";
 import BarChart from "../charts/BarChart";
 import { AppData } from "../charts/Data";
 import PieChart from "../charts/PieChart.js";
 import './home.css'
+import * as firebase from "firebase/app";
+import { QuerySnapshot } from "firebase/firestore";
 
 const Home = () => {
-  const [appData, setUserData] = useState({
-    labels: AppData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: AppData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#b2e0ed",
-          "#4AB4A1",
-          "#F14C56",
-          "#2a71d0",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  });
+  const ref = firebase.initializeApp().collection("info")
+  console.log(ref);
+
+  const [data, setData] = useState([])
+  const [loader, setLoader] = useState(true)
+
+  const getData = () => {
+    ref.onSnapshot((QuerySnapshot)=>{
+      const items = []
+      QuerySnapshot.forEach((doc)=>{
+        items.push(doc.data())
+      })
+      setData(items)
+      setLoader(false)
+    })
+  }
+  useEffect(()=>{
+    getData()
+    console.log(data);
+  },[])
 
   return (
     <div className="charts">
       <Cards />
+      {loader === false && (data.map((year)=>{
+        <div key = {year.id}> 
       <div className="chartcontainer">
         <div style={{ width: 500, marginLeft:70}}>
-          <BarChart chartData={appData} />
+          <BarChart chartData={data} />
         </div>
         <div style={{ width: 280, marginLeft:20, }}>
-          <PieChart chartData={appData} />
+          <PieChart chartData={data} />
         </div>
       </div>
+      </div> 
+      }))} 
     </div>
   );
 };
