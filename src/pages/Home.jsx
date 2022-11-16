72bc058ec3cfad9c372c69ede9c016ad02ccda7f
Natete-Verite <nateteverite@gmail.com>
2022-11-16 21:36:13 +0300

    fetching data from firestore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,65 @@
 import React, { useState, useEffect } from "react";
 import Cards from "../components/Cards";
 import BarChart from "../charts/BarChart";
-import { AppData } from "../charts/Data";
 import PieChart from "../charts/PieChart.js";
 import './home.css'
-import * as firebase from "firebase/app";
-import { QuerySnapshot } from "firebase/firestore";
+import {db} from "../firebase";
+import { QuerySnapshot, collection, doc, getDoc } from "firebase/firestore";
+// import { async } from "@firebase/util";
 
 const Home = () => {
-  const ref = firebase.initializeApp().collection("info")
-  console.log(ref);
-
-  const [data, setData] = useState([])
-  const [loader, setLoader] = useState(true)
-
-  const getData = () => {
-    ref.onSnapshot((QuerySnapshot)=>{
-      const items = []
-      QuerySnapshot.forEach((doc)=>{
-        items.push(doc.data())
-      })
-      setData(items)
-      setLoader(false)
-    })
-  }
+  
+ 
+  // console.log(docRef, "inform");
   useEffect(()=>{
-    getData()
-    console.log(data);
+    const docRef = collection(db, "info");
+    const information = async ()=>{
+      const docSnap = await getDoc(docRef)
+      if (docSnap.exists()){
+        console.log("Document data:", docSnap.data());
+      }else{
+        console.log("No such document");
+      }
+    }
+    information()
+    console.log("information");
   },[])
+  
+
+  // const [data, setData] = useState([])
+  // const [loader, setLoader] = useState(true)
+
+  // const getData = () => {
+  //   ref.onSnapshot((QuerySnapshot)=>{
+  //     const items = []
+  //     QuerySnapshot.forEach((doc)=>{
+  //       items.push(doc.data())
+  //     })
+  //     setData(items)
+  //     setLoader(false)
+  //   })
+  // }
+  // useEffect(()=>{
+  //   getData()
+  //   console.log(data);
+  // },[])
 
   return (
     <div className="charts">
       <Cards />
-      {loader === false && (data.map((year)=>{
-        <div key = {year.id}> 
+      {/* {docSnap === false && (docSnap.map((year)=>{ */}
+         
       <div className="chartcontainer">
         <div style={{ width: 500, marginLeft:70}}>
-          <BarChart chartData={data} />
+          {/* <BarChart chartData={docSnap} /> */}
         </div>
         <div style={{ width: 280, marginLeft:20, }}>
-          <PieChart chartData={data} />
+          {/* <PieChart chartData={docSnap} /> */}
         </div>
       </div>
       </div> 
-      }))} 
-    </div>
+      
+  
   );
 };
 
